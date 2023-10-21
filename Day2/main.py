1d554c4c791833eafc37f8e7d1a21089c79b1960
Karthik K <nathandrake@Nathans-MBP.lan>
2023-10-20 21:20:28 -0500

    Did How to take inputs from users in python

--- Day2/main.py
+++ Day2/main.py
@@ -23,6 +23,28 @@ def main():
 
     print(book + ' is a novel by' + author + ', published in ' + str(release_year) + '. It has a rating of ' + str(goodreads_rating) + ' on goodreads.')
 
+    num_1 = 15
+    num_2 = 12
+
+    print(f'sum of num_1 and num_2 is: {num_1 + num_2}')
+    print(f'difference of num_1 and num_2 is: {num_1 - num_2}')
+    print(f'difference of num_2 and num_1 is: {num_2 - num_1}')
+    print(f'product of num_1 and num_2 is: {num_1 * num_2}')
+    print(f'quotient of num_1 and num_2 is: {num_1 / num_2}')
+    print(f'floored quotient of num_1 and num_2 is: {num_1 // num_2}')
+
+    float_variable = 1.25
+    integer_variable = 55
+
+    print(f'{float_variable} converted to an integer is : {int(float_variable)}')
+    print(f'{integer_variable} converted to a float is: {float(integer_variable)}')
+
+    #How to take inputs from users in python
+
+    name = input('What is your name?')
+
+    print(f'Nice to meet you {name}')
+
 if __name__ == '__main__':
     main()
 
