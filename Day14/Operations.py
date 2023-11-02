41b40bdf9af2d03d61ee71e019f71875bd178276
Karthik <karthikkadapa88@gmail.com>
2023-11-01 21:33:20 -0500

    Day 13 "Mathematical Operations"

---
+++ Day14/Operations.py
@@ -0,0 +1,14 @@
+
+def main():
+    print('Order of execution of Mathematical Operators in Python')
+    print('PEMDAS')
+    print('Parenthesis ()')
+    print('Exponents **')
+    print('Multiplication *')
+    print('Division /')
+    print('Addition +')
+    print('Subtraction -')
+
+
+if __name__ == '__main__':
+    main()
