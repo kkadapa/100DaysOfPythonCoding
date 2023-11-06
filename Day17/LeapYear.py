2f7980c0cb4bdf359fdbda409c09913125dc359c
Karthik <karthikkadapa88@gmail.com>
2023-11-05 20:23:10 -0600

    Day 17 "Leap Year or not"

---
+++ Day17/LeapYear.py
@@ -0,0 +1,19 @@
+
+def main():
+    print('Welcome to Leap year!')
+
+    year = int(input('Which year do you want to check?'))
+
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('Leap Year')
+            else:
+                print('Not Leap Year')
+        else:
+            print('Leap Year')
+    else:
+        print('Not Leap Year')
+
+if __name__ == '__main__':
+    main()
