62125c095c35da77fcfc072d023cbb99f5decce7
Karthik <karthikkadapa88@gmail.com>
2023-11-09 20:49:47 -0600

    Day 21 "Treasure Island Hunt1"

--- Day21/TreasureIslandEnd.py
+++ Day21/TreasureIslandEnd.py
@@ -4,14 +4,23 @@ def main():
 
     print('Your mission is to find treasure.')
 
-    step1 = input('Left or Right?')
+    step1 = input('Left or Right?\n')
 
     if step1 == 'Right':
         print('Game Over!')
     else:
-        step2 = input('Swim or Wait?')
+        step2 = input('Swim or Wait?\n')
         if step2 == 'Swim':
             print('Game Over')
+        else:
+            step3 = input('Which door Red or Blue or Yellow?\n')
+            if step3 == 'Red':
+                print('Game Over')
+            elif step3 == 'Blue':
+                print('Game Over')
+            else:
+                print('You Win!')
+
 
 if __name__ == '__main__':
     main()
