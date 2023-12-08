d4f5099160a3fd07e0a1f4561debd2a6ada146fc
Karthik <karthikkadapa88@gmail.com>
2023-12-08 08:23:23 -0600

    Day 27 "Average Array Test"

---
+++ Day27/ArrayTest.py
@@ -0,0 +1,12 @@
+
+us_states = [
+    'Andhra Pradesh', 'Telangana', 'Punjab'
+]
+
+def main():
+    print('Array Test.')
+    print(f'Length of array',len(us_states))
+    print(us_states.count('Telangana'))
+
+if __name__ == '__main__':
+    main()
